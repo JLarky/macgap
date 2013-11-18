2e495585f471593fce7a1de8b6b1d3222dd59722
Peter Hizalev <peter.hizalev@gmail.com>
2013-11-18 10:01:21 -0800

    Katoified

--- Kato/Classes/WebViewDelegate.h
+++ Kato/Classes/WebViewDelegate.h
@@ -16,7 +16,6 @@
 @interface WebViewDelegate : NSObject {
 	Sound* sound;
     Dock* dock;
-    Growl* growl;
     Notice* notice;
     Path* path;
     Localfile* localfile;
@@ -29,7 +28,6 @@
 
 @property (nonatomic, retain) Sound* sound;
 @property (nonatomic, retain) Dock* dock;
-@property (nonatomic, retain) Growl* growl;
 @property (nonatomic, retain) Notice* notice;
 @property (nonatomic, retain) Path* path;
 @property (nonatomic, retain) Localfile* localfile;
